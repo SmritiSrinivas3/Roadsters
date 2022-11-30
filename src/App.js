9a76d5eadb6ebd4f710a938da2bc8cc31764737e
Rahul v s <rahulvs037@gmail.com>
2022-11-30 20:37:38 +0530

    error correction

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
-import Profile from './pages/Profile/Profile';
-import Home from './pages/Profile/homepage';
+import UserDetails from './components/Userdetails/Userdetails'
+import Sidenav from './components/Sidenav/Sidenav';
+import MyOrders from './components/MyOrders/MyOrders';
+
 function App() {
   return (
     <div className="App">
-    <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            
-          </Routes>
-     
+      <Routes>
+        <Route path="/profile" element={<>
+          <Sidenav />
+          <UserDetails />
+        </>
+        } ></Route>
+        <Route path='/myorders' element={
+          <>
+            <Sidenav />
+           <MyOrders/>
+          </>
+        }></Route>
+      </Routes>
     </div>
   );
 }
