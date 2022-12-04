8d7897d3d0155286bafc8409bd35a50a52ab4b58
SmritiSrinivas3 <smritisrinivasneelamana@gmail.com>
2022-12-04 15:34:17 +0530

    profile image loaded from the database

--- src/components/Userdetails/Userdetails.js
+++ src/components/Userdetails/Userdetails.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { db } from '../../utils/firebase'
 import { getDocs, collection } from 'firebase/firestore'
 import './Userdetails.css'
-import profileImg from './profile.png'
+
 
 
 export default function Userdetails() {
@@ -13,7 +13,7 @@ export default function Userdetails() {
         let userData = []
         const userDetailsFirebase = await getDocs(collection(db, 'userDetails'))
         userDetailsFirebase.forEach((user) => {
-          userData.push({id: user.id, ...user.data()})
+          userData.push({ id: user.id, ...user.data() })
         })
         setData(userData)
         console.log(data)
@@ -30,19 +30,21 @@ export default function Userdetails() {
       <h1>My Details</h1>
       <h2>Personal Information</h2>
       <hr />
-      <br/>
-      <br/>
-      <img src={profileImg}></img>
-      <div className='Info'>
-      {data.map((entry)=>(
+      <br />
+      <br />
+      {data.map((entry) => (
         <div>
-        <h4 id={entry.id}><b>Name:</b>   {entry.Name}</h4> 
-        <h4 id={entry.id}> <b>Email: </b>   {entry.Email}</h4>
-        <h4 id={entry.id}> <b>Phone number:</b>    {entry.phoneNumber}</h4>
-        </div> 
+          <img src={entry.profileImg}></img>
+          <div className='Info'>
+
+            <h4 id={entry.id}><b>Name:</b>   {entry.Name}</h4>
+            <h4 id={entry.id}> <b>Email: </b>   {entry.Email}</h4>
+            <h4 id={entry.id}> <b>Phone number:</b>    {entry.phoneNumber}</h4>
+          </div> 
+          </div>
       ))}
-      
-      </div>
+
+        
     </div>
   )
 }
